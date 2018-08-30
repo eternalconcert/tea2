25bcdb5fdf8a3c1860f32b23af6d949d10535892
xgwschk <devnull@localhost>
2018-08-30 17:19:35 +0200

    Stuff

--- srcsimple/ast.cpp
+++ srcsimple/ast.cpp
@@ -2,8 +2,47 @@
 #include "exceptions.h"
 
 
-enum TYPE_ID {INT, FLOAT, STR, BOOL, VOID, ARRAY, IDENTIFIER};
+enum typeId {INT, FLOAT, STR, BOOL, VOID, ARRAY, IDENTIFIER};
 
 
 class AstNode {
+
+};
+
+
+class Constant {
+public:
+    typeId type;
+    std::string ident;
+
+    int intValue;
+    float floatValue;
+    char *stringValue;
+    char *boolValue;
+    void repr() {
+        switch (this->type) {
+            case INT:
+                printf("%d\n", intValue);
+                break;
+
+            case FLOAT:
+                printf("%f\n", floatValue);
+                break;
+
+            case STR:
+                printf("%s\n", stringValue);
+                break;
+
+            case BOOL:
+                printf("%s\n", boolValue);
+                break;
+        };
+    };
+};
+
+
+class Assignment: public AstNode {
+public:
+    AstNode leftValue;
+    AstNode rightValue;
 };
